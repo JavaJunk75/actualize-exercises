46cf7d1ae8856dd86fcf1838b05b375086f9911d
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-17 00:32:06 -0800

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,23 @@
+puts 40 / 10
+puts 10 * 3
+puts 5 + 7
+puts 990 - 7
+
+puts 40 x 10
+Puts 10 * 3
+PUTS 5 + 7
+puts990-7
+
+# Error msg from try ruby line 6 <anonymous>': : eval:6: syntax error, unexpected local variable or method, expecting end-of-input
+puts 40 x 10
+        ^
+ (SyntaxError)
+# error msg from line 7 <anonymous>': eval:1:in `<main>': undefined method `Puts' for main:Object (NoMethodError)
+Type 2 + 6 in the editor
+# error msg line 8  <anonymous>': eval:1:in `<main>': undefined method `PUTS' for main:Object (NoMethodError)
+# error msg line 9 <anonymous>': eval:1:in `<main>': undefined local variable or method `puts990' for main:Object (NameError)
+
+# error due to using an x instead of *
+# error due to capitol P
+# error due to all caps
+# error due to no space between the word puts and the number 990
