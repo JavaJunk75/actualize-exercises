2579919c399ed7e15f39eba4628cd2d33c7cf0d3
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-18 19:56:38 -0800

    Update 112.rb

--- 112.rb
+++ 112.rb
@@ -1,18 +1,9 @@
 puts "hellohellohello"
 puts "hello" * 3 
 
-puts "9 + 3"
-puts "9 + 3"
-puts "9 + 3"
-puts "9 + 3"
-puts "9 - 3"
-puts "9 - 3"
-puts "9 - 3"
-puts "9 * 3"
-puts "9 * 3"
-puts "9 * 3"
-puts "9 * 3"
-puts "9 / 3"
-puts "9 / 3"
-puts "9 / 3"
-puts "9 / 3"
+puts 9 + 3
+puts "9" + "3"
+puts 9 - 3
+puts 9 * 3
+puts "9" * 3
+puts 9 / 3
