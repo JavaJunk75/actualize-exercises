a2df50bf09863ca380f180fe7003c052310b8118
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-20 10:08:33 -0800

    121.rb

---
+++ 121.rb
@@ -0,0 +1,5 @@
+" now  is  the  time  to ".squeeze  " the bell rang for class at 8am".squeeze #squeeze method removed duplicate letters and removed extra spaces in a string
+"KENNEDY SpAcE center".swapcase #converts any lowercase characters into uppercase and uppercase into lowercase
+"actualize bootcamp".chars #separates each character within the string
+
+He >= I #the method cant be used with words
