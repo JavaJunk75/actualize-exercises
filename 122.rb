8edc4609855abc85373b9d99ccc11e3d644aa566
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-20 11:27:23 -0800

    122.rb

---
+++ 122.rb
@@ -0,0 +1,12 @@
+puts "What is your name?" + "My name is Tori." 
+puts "What is your favorite color?" + "My favorite color is blue."
+
+****Pending assistance via Slack - message sent 01/20 saving for now to move on to next exercise****
+# The following code has a mistake on one line that doesn't cause the program to crash,
+# but it causes the program to have the wrong output.
+# Explain where the mistake is occurring, then fix the code
+# (this may require changing more than one line).
+puts "Please enter a number:"
+number = gets.chomp
+doubled_number = number * 2
+puts "The doubled number is " + doubled_number
