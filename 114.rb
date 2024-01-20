85e6471706da5afe65ba1369e2e38b805fe457cf
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-19 21:54:10 -0800

    114.rb

---
+++ 114.rb
@@ -0,0 +1,18 @@
+#Z would equal 43 
+#The values were printed to the teminal because z = x + y (43) and the puts z lines would print what z equals.
+x = 40
+y = 3
+z = x + y
+puts z
+x = 60
+puts z
+
+# Write comments for the remaining lines to describe in your own words 
+# what is happening line by line with the following code.
+mars = 4               # Make a variable called mars and set the value to 4
+mars = 2 * mars        # Mars equals 2 x mars (4)
+pluto = 7              # variable pluto equals 7
+mars = mars - pluto    # mars equals mars (8) minus pluto (7)
+pluto = pluto + mars   # pluto equals pluto (7) plus mars (1)
+puts mars              # mars equals 1
+puts pluto             # pluto equals 8
