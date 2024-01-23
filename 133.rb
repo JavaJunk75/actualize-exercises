44af4643d57cc5df16a4bd0ada1daedb7787fa4a
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-22 21:06:55 -0800

    133.rb

---
+++ 133.rb
@@ -0,0 +1,38 @@
+number = 1
+
+while number < 20
+  if number == 7
+    puts "Hey, that's my lucky number!"
+  elsif
+    puts number 
+  elsif
+    number == 15    
+      puts "Hey, that's my lucky number!"
+    end
+   puts number = number + 1
+end
+
+
+
+# QUESTION 2
+# Finish annotating the code below.
+# Then run the code to see if the output matches your annotation.
+number = 0             # 1
+while 2 > number       # 2 
+  if number < 1        # 
+    puts number        #  
+  end
+  number = number + 1  # 
+end
+puts "Done!"           # 
+
+# 1: number is 0
+# 2: (2 is greater than 0, continue)
+# 3: 
+# 4: 
+# 5: 
+# 6: 
+# 7: 
+# 8: 
+# 9: 
+# 10: 
