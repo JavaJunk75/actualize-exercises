df99ed7264702371e8c3f6477193ee85e5df5ea6
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-24 18:04:10 -0800

    Update 134.rb

--- 134.rb
+++ 134.rb
@@ -10,17 +10,3 @@ while true
         break
     end
 end
-
-x = 0                                  # 1
-names = ["Saron", "Majora", "Danilo"]  # 2
-names[1] = "Bob"                       # 3
-x = names[2]                           # 4
-names[0] = x                           # 5
-pp names                               # 6
-
-# 1: x is 0
-# 2: x is 0, names is ["Saron", "Majora", "Danilo"]
-# 3: x is 0, names is ["Saron", "Bob", "Danilo"]
-# 4: x is "Danilo", names is ["Saron", "Majora", "Danilo"]
-# 5: x is "Danilo", names is ["Danilo", "Majora", "Danilo"]
-# 6: x is "Danilo", names is ["Danilo", "Majora", "Danilo"]
