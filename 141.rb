8091b8f2d9697eba0166a9cd5eae8f8ed5c1f93b
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-24 18:03:13 -0800

    Update 141.rb

--- 141.rb
+++ 141.rb
@@ -2,13 +2,12 @@ x = 0                                  # 1
 names = ["Saron", "Majora", "Danilo"]  # 2
 names[1] = "Bob"                       # 3
 x = names[2]                           # 4
-names[0] = x                           # 
-pp names                               # 
+names[0] = x                           # 5
+pp names                               # 6
 
 # 1: x is 0
 # 2: x is 0, names is ["Saron", "Majora", "Danilo"]
-# 3: the second name was changed from Majora to Bob
-# 4: Sharon item 0 was changed out to the item 2 in the bracket Danilo  
-
-items = [3, 2, 3, 2]
-pp items
+# 3: x is 0, names is ["Saron", "Bob", "Danilo"]
+# 4: x is "Danilo", names is ["Saron", "Majora", "Danilo"]
+# 5: x is "Danilo", names is ["Danilo", "Majora", "Danilo"]
+# 6: x is "Danilo", names is ["Danilo", "Majora", "Danilo"]
