14badf6864d3d2065465049db332a2afaaa6e4bf
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-25 21:47:05 -0800

    142.rb

---
+++ 142.rb
@@ -0,0 +1,14 @@
+numbers = [10, 7, 8, 25]
+index = 0
+while index < numbers.length
+    puts numbers [index]
+index = index + 1
+end
+
+numbers = [10, 7, 8, 25]
+index = 0
+while index < numbers.length
+  puts numbers[index]
+  index = index + 1
+end
+#Need assistance with this code. Will schedule 1:1
