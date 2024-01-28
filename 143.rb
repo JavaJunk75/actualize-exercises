2361eb267f5363be408e586e42af443122fb3742
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-27 17:43:55 -0800

    143.rb

---
+++ 143.rb
@@ -0,0 +1,9 @@
+city_populations = { "Chicago" => 2700000 }  
+city_populations["New York City"] = 8,400,000  
+city_populations ["San Francisco"] = 800,000
+pp city_populations
+
+my_pets = { "dogs" => 2, "cats" => 1, "fish" => 5 }
+if my_pets[2] = "dogs"          #The double = caused the code not to work
+  puts "I have 2 dogs!"
+end
