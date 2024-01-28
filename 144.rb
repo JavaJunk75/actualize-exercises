47a9346c2d249351e60b306724e45dc4b77818a4
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-27 18:19:58 -0800

    144.rb

---
+++ 144.rb
@@ -0,0 +1,12 @@
+cars = [
+{"make" => "toyota", "model" => "solara", "color" => "red", "year" => 2004},
+{"make" => "Nissan", "model" => 350, "color" => "silver", "year" => 2002},
+{"make" => "Kia", "model" => "sportage", "color" => "white", "year" => 2023}]
+
+
+students = [
+  { "name" => "Maria", "age" => 20, "major" => "Computer Science" },
+  { "name" => "Sofia", "age" => 21, "major" => "Mathematics" },
+  { "name" => "Jayden", "age" => 19, "major" => "Biology" }
+]
+puts students[0]["age"]
