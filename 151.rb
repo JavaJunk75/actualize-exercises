90f7cf86f8d1192f556f376ef8f853748f2e5e45
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-27 19:33:32 -0800

    151.rb

---
+++ 151.rb
@@ -0,0 +1,24 @@
+def display_welcome_screen
+puts  "___ ___         _____            _____       .___      .__" 
+puts "/   |   \_____ _/ ____\____      /  _  \    __| _/____  |__|"
+puts"/    ~    \__  \\   __\\__  \    /  /_\  \  / __ |\__  \ |  |"
+puts"\    Y    // __ \|  |   / __ \_ /    |    \/ /_/ | / __ \|  |"
+puts "\___|_  /(____  /__|  (____  / \____|__  /\____ |(____  /__|"
+puts "      \/      \/           \/          \/      \/     \/"    
+end
+
+display_welcome_screen
+
+display_welcome_screen
+
+display_welcome_screen
+
+
+def print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+  end
+
+print_lyrics 
