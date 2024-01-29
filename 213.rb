868b483255e5eefba28817637d7bb3a1c9910305
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-28 21:38:06 -0800

    213.rb

---
+++ 213.rb
@@ -0,0 +1,9 @@
+first_name = "Katherine"
+last_name = "Johnson"
+puts first_name + " " + last_name + " was a NASA research mathematician."
+puts "#{first_name + last_name}, was a NASA research mathematician." 
+
+
+name = "Salvador"
+age = 37.to_s
+puts name + "is" + age + "years old and goes by Chava with his friends."
