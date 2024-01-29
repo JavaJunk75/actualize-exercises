8b7ab8616f6523c3e38ae7f313118e420e7b0bfc
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-01-28 16:04:32 -0800

    211.rb

---
+++ 211.rb
@@ -0,0 +1,7 @@
+git add --all
+git commit -m "Add exercise solutions"
+git push origin main
+
+#Q1 I apologize,I need more practice. I do not understand this code.
+
+#Q2 This did not come easily and I was not able to solve from memory.
