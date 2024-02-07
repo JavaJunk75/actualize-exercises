41b954d642129f5a1196d6a3a523c8210ba3dbf1
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-06 23:56:36 -0800

    411.rb

---
+++ 411.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#Write a program that uses a variable to store a number, then prints the number times 10.
+
+10.times do
+puts "eric" 
+end
