3167e162ad346e8c7c3cfef59cde58a2f992a88a
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-06 19:43:40 -0800

    311.rb

---
+++ 311.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+number1 = 7
+number2 = 25
+puts number1 + number2
+
+#Yes, was able to complete easily from memory.
+#Will start on the next problem
