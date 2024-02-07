4321f01e39ec2977915ad92524b31786d7502485
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-06 22:40:05 -0800

    312.rb

---
+++ 312.rb
@@ -0,0 +1,8 @@
+# QUESTION 1
+# Use a loop to print out only the numbers greater than 10.
+numbers = [4, 53, 22, 3, 9, 7, 12]
+numbers.each do |number|
+  puts number > 10
+end
+
+#outcome is printing out only as true or false. How can I fix this?
