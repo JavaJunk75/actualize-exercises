0dfe2722395a8f8f0b1b5e1c851a8739e4a790d1
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-07 21:48:17 -0800

    241.rb

---
+++ 241.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+test = "mouse pad"
+pp test.upcase
+
+#No, I wasn't able to complete the problem from memory. I needed to review the code.
