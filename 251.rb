1dc57c243cf497ee93c31dfbb36c5fd38908061a
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-09 17:22:00 -0800

    251.rb

---
+++ 251.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#Write a program that uses a variable to store a word, then prints the number of letters in the word.
+son = "Aiden"
+pp son.length
+
+#no, I was not able to solve off of memory. I forgot the mothod.
