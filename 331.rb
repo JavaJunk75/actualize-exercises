d44110fb75f5b071c28d6e33296e1b3ab9b488d7
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-10 20:48:31 -0800

    331.rb

---
+++ 331.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+cost = 17.99
+puts cost.to_i
+
+#No, I was not able to complete off of memory. With more practice should be able to retain.
