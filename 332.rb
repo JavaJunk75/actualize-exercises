8a1c886ce19facceeccba34085b00f2301034a73
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-10 21:34:49 -0800

    332.rb

---
+++ 332.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Use a loop to compute the sum of all the given numbers.
+numbers = [5, 4, 2, 2, 6, 8, 1]
+
+index = 0 
+while index < numbers.length
+  number = numbers[index]
+  pp number
+  index = index + 1
+end
