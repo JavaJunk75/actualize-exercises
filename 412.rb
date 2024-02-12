9ef90cafdce84ecd8a0b0e068dd3323b2f3e5af5
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-11 22:35:05 -0800

    412.rb

---
+++ 412.rb
@@ -0,0 +1,11 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns its sum.
+# Then run the method and print the result.
+
+
+numbers = (48, 77, 25)
+sum = 0
+return array.each { |a| sum+=a }
+
+#please let me know what exercise video covers the sum. I need to rewatch and refresh my memory. 
+#I apologize, trying to get caught up. Moving on to the next lesson. Will correct.
