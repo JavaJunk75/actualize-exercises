197ce1a2b06be17514f5730a5480fcaa5425a94a
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-12 22:15:03 -0800

    413.rb

---
+++ 413.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Read about the Ruby File class: https://rubyapi.org/3.2/o/s?q=file
+# Then write a program that creates a file called example.txt with the 
+# sentence "This is an example!" in it.
+
+
+example.txt('t.txt, text')
+file.writa('t.txt')
+
+= File.new('t.dat', wb: UT16 ')
