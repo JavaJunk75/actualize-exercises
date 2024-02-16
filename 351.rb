9d7c62f953f93ecdc587686f161d6ad788cbfa99
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-15 23:39:05 -0800

    351.rb

---
+++ 351.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+#Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines
+
+person = { "First Name" =>  "Eric", "Last Name" => "Cruz", "Email" => "eric@gmail.com" }
+pp person["First Name"]
+pp person["Last Name"]
