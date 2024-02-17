c64345f4aca78a317c66ac36df3d3d7b0659c9c4
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-17 07:14:50 -0800

    353.rb

--- 353.rb
+++ 353.rb
@@ -1,1 +1,15 @@
+# QUESTION 1
+# Start your program with the line of code below.
+# The rest of your program should print out the second letter from each inner array. 
+# (The result in your terminal should be the letters b, d, f, h, j on separate lines)
+letters = [["a", "b"], ["c", "d"], ["e", "f"], ["g", "h"], ["i", "j"]]
 
+letters = [["a", "b"], ["c", "d"], ["e", "f"], ["g", "h"], ["i", "j"]]
+second_letters = []
+index = 0
+while index < letters.length
+  letter = letters[index]
+  second_letters << letters[0]
+  index = index + 1
+end
+p second_letters
