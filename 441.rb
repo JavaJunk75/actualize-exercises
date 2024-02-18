afa78624828cbb78b668c63498234d53b4578132
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-17 20:50:48 -0800

    441.rb

---
+++ 441.rb
@@ -0,0 +1,8 @@
+#Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [8, 23]
+index = 0
+while index < numbers.length
+  pp numbers
+index = index + 1
+end
