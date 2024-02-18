a0499ee0ab42fc69185c5dfc016751e7cf73fea4
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-17 18:15:39 -0800

    432.rb

---
+++ 432.rb
@@ -0,0 +1,7 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns the mean average.
+# Then run the method and print the result.
+
+numbers = [ 9, 15, 20 ]
+numbers.sum()/numbers.size
+pp numbers
