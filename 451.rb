5aef257d6b0fc15c89cad8b8617fc9343f6745b5
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-17 21:27:00 -0800

    451.rb

---
+++ 451.rb
@@ -0,0 +1,4 @@
+#Create an array to store names of 3 different countries. Then add one more country and print the array one line.cou
+countries = ["Italy", "Australia", "France"]
+countries.push("United States")
+pp countries
