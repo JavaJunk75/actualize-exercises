adc77d90cd97c2504a30fb2f5976d0dc52a327cd
valarielujan <156874955+JavaJunk75@users.noreply.github.com>
2024-02-17 17:42:15 -0800

    424.rb

---
+++ 424.rb
@@ -0,0 +1,9 @@
+# QUESTION 1
+# Read about the Ruby map method: https://www.rubyguides.com/2018/10/ruby-map-method/ 
+# Then write a program that uses the map method with an array of numbers 
+# to create a new array with each number multiplied by 100.
+
+
+array.map { 1, 99, 17, 34 }
+100.times do
+pp array.map
